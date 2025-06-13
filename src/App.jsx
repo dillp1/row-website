2176888fadcca82caf92a06c3690f10b0d317310
Dillon Michlena <144554032+dillp1@users.noreply.github.com>
2025-06-13 17:01:12 -0400

    navbar, hero and demands

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./vars.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="text-white bg-custom min-h-screen">
+      {/* Header Section */}
+      <div className="flex flex-row justify-between items-center px-[150px] py-[70px] text-[32px]">
+        <p className="font-bold">ROW</p>
+        <div className="flex flex-row gap-[50px]">
+          <p>Demands</p>
+          <p>Intimidation</p>
+          <p>Press</p>
+          <p>FAQ</p>
+          <p>Contact</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      {/* Hero Section */}
+      <div className="flex flex-row justify-between items-center px-[150px]">
+        <div className="flex flex-col space-y-[25px]">
+          <p className="text-[96px] font-bold leading-tight">
+            ResLife Organized Workers
+          </p>
+          <p className="text-[24px]">
+            Representing all Resident Assistants and Residential Education
+            Assistants at Purdue University
+          </p>
+          <div className="flex flex-row items-center gap-[15px]">
+            <button className="btn-primary-yellow text-black w-[275px] h-[80px] rounded-[40px] text-[24px]">
+              About Us
+            </button>
+            <button className="btn-secondary-yellow text-white w-[275px] h-[80px] rounded-[40px] text-[24px]">
+              Sign A Card
+            </button>
+          </div>
+        </div>
+        <img src="/src/assets/row-logo.png" className="" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {/* Demands Section */}
+      <div className="flex flex-col justify-between px-[150px]">
+        <p className="text-[64px] font-semibold">We Demand...</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[16px] text-black text-[20px]">
+          <div className="demand-box flex flex-row gap-[16px] rounded-xl p-6">
+            <div className="w-[70px] h-[70px] flex-shrink-0 flex items-center justify-center">
+              <img
+                src="/src/assets/MetNotification.svg"
+                className="w-[70px] h-[70px]"
+              />
+            </div>
+            <p>
+              ... that any ResLife student staff never be assigned to share a
+              living space with residents or a bedroom with any other
+              individuals.
+            </p>
+          </div>
+          <div className="demand-box flex flex-row gap-[16px] rounded-xl p-6">
+            <div className="w-[70px] h-[70px] flex-shrink-0 flex items-center justify-center">
+              <img
+                src="/src/assets/MetNotification.svg"
+                className="w-[70px] h-[70px]"
+              />
+            </div>
+            <p>
+              ... that any ResLife student staff never be assigned to share a
+              living space with residents or a bedroom with any other
+              individuals.
+            </p>
+          </div>
+          <div className="demand-box flex flex-row gap-[16px] rounded-xl p-6">
+            <div className="w-[70px] h-[70px] flex-shrink-0 flex items-center justify-center">
+              <img
+                src="/src/assets/AddressedNotification.svg"
+                className="w-[70px] h-[70px]"
+              />
+            </div>
+            <p>
+              ... that any ResLife student staff never be assigned to share a
+              living space with residents or a bedroom with any other
+              individuals.... that any ResLife student staff never be assigned
+              to share a living space with residents or a bedroom with any other
+              individuals.... that any ResLife student staff never be assigned
+              to share a living space with residents or a bedroom with any other
+              individuals.
+            </p>
+          </div>
+          <div className="demand-box flex flex-row gap-[16px] rounded-xl p-6">
+            <div className="w-[70px] h-[70px] flex-shrink-0 flex items-center justify-center">
+              <img
+                src="/src/assets/AddressedNotification.svg"
+                className="w-[70px] h-[70px]"
+              />
+            </div>
+            <p>
+              ... that any ResLife student staff never be assigned to share a
+              living space with residents or a bedroom with any other
+              individuals.... that any ResLife student staff never be assigned
+              to share a living space with residents or a bedroom with any other
+              individuals.... that any ResLife student staff never be assigned
+              to share a living space with residents or a bedroom with any other
+              individuals.
+            </p>
+          </div>
+          <div className="demand-box flex flex-row gap-[16px] rounded-xl p-6">
+            <div className="w-[70px] h-[70px] flex-shrink-0 flex items-center justify-center">
+              <img
+                src="/src/assets/UnaddressedNotification.svg"
+                className="w-[70px] h-[70px]"
+              />
+            </div>
+            <p>
+              hello world this demand has not been addressed by Purdue how sad
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
