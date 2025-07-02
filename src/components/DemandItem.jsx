82187c18edbd1e7a3684a2f9b7c3ebfb6fd7be6d
dillp1 <dillon.michlena@gmail.com>
2025-07-02 14:53:46 -0400

    added demands

--- src/components/DemandItem.jsx
+++ src/components/DemandItem.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function DemandItem({ icon, text }) {
   return (
-    <div className="flex flex-row gap-[15px]">
+    <div className="flex flex-row items-center gap-[15px]">
       <img src={icon} alt=""/>
-      <p className="lg:text-[22px]">{text}</p>
+      <p className="text-[18px] sm:text-[20px] lg:text-[24px]">{text}</p>
     </div>
   );
 }
--- src/components/Demands.jsx
+++ src/components/Demands.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import DemandItem from "./DemandItem";
 
 import metIcon from "../assets/MetNotification.svg";
-import addressedIcon from "../assets/AddressedNotification.svg";
+/* import addressedIcon from "../assets/AddressedNotification.svg"; */
 import unaddressedIcon from "../assets/UnaddressedNotification.svg";
 
 function Demands() {
@@ -20,24 +20,48 @@ function Demands() {
           text="... that any ResLife student staff never be assigned to share a living space with residents or a bedroom with any other individuals."
         />
         <DemandItem
-          icon={metIcon}
-          text="... that any ResLife student staff never be assigned to share a living space with residents or a bedroom with any other individuals."
-        />
-        <DemandItem
-          icon={addressedIcon}
-          text="... that any ResLife student staff never be assigned to share a living space with residents or a bedroom with any other individuals."
-        />
-        <DemandItem
-          icon={addressedIcon}
-          text="... that any ResLife student staff never be assigned to share a living space with residents or a bedroom with any other individuals."
+          icon={unaddressedIcon}
+          text="... a guaranteed severance package for any ResLife student staff who resigns or is fired. This should include, but not be limited to, the option of housing and a meal plan for the remainder of the given academic year."
         />
         <DemandItem
           icon={unaddressedIcon}
-          text="... that any ResLife student staff never be assigned to share a living space with residents or a bedroom with any other individuals."
+          text="... any ResLife student staff required to use personal vehicles to do their job in a timely manner be provided vehicle reimbursements consistent with the IRS standard mileage rates."
         />
         <DemandItem
           icon={unaddressedIcon}
-          text="... that any ResLife student staff never be assigned to share a living space with residents or a bedroom with any other individuals."
+          text="... any ResLife student staff required to live in housing without air conditioning be provided with box fans."
+        />
+        <DemandItem
+          icon={unaddressedIcon}
+          text="... all ResLife student staff be guaranteed free parking."
+        />
+        <DemandItem
+          icon={unaddressedIcon}
+          text="... that contracts will move from at-will employment to just-cause employment, and will not change without mutual agreement after being signed and accepted."
+        />
+        <DemandItem
+          icon={unaddressedIcon}
+          text="... every ResLife student staff member be able to choose which meal plan they want."
+        />
+        <DemandItem
+          icon={unaddressedIcon}
+          text="... at most 40 residents be assigned to any given RA."
+        />
+        <DemandItem
+          icon={unaddressedIcon}
+          text="... at least one mental health counselor to be on call for all ResLife student staff who is not a Title IX mandatory reporter."
+        />
+        <DemandItem
+          icon={unaddressedIcon}
+          text="... to no longer be compelled to work front desk shifts without additional compensation."
+        />
+        <DemandItem
+          icon={unaddressedIcon}
+          text="... transparency regarding traumatic events and deaths within the community."
+        />
+        <DemandItem
+          icon={unaddressedIcon}
+          text="... free laundry for all ResLife student staff."
         />
       </div>
     </section>
