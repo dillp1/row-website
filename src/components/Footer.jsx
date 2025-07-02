0cdd56fd4b64a63e511fb6e22d37b5e3274fbb44
dillp1 <dillon.michlena@gmail.com>
2025-07-02 10:44:25 -0400

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ import mailIcon from "../assets/mail.svg";
 
 function Footer() {
   return (
-    <footer className="footer flex flex-row justify-between items-center px-[150px]">
-      <p className="text-[64px] font-semibold">ROW</p>
-      <div className="flex flex-row gap-[10px]">
-        <img src={instagramIcon} />
-        <img src={mailIcon} />
+    <footer className="footer flex flex-col sm:flex-row justify-between items-center px-6 py-4 md:px-[75px] lg:px-[150px] gap-4 sm:gap-0">
+      <p className="text-[40px] sm:text-[48px] lg:text-[64px] font-semibold">ROW</p>
+      <div className="flex flex-row gap-[20px]">
+        <img src={instagramIcon} className="w-8 h-auto lg:w-16 lg:h-auto"/>
+        <img src={mailIcon} className="w-8 h-auto lg:w-16 lg:h-auto"/>
       </div>
     </footer>
   );
