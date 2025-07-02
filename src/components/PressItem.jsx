edfec127216250ac1509bc27173168d4240f5d0e
dillp1 <dillon.michlena@gmail.com>
2025-07-02 16:37:04 -0400

    fixed press items wrap issues

--- src/components/PressItem.jsx
+++ src/components/PressItem.jsx
@@ -3,11 +3,16 @@ import React from "react";
 function PressItem({ day, date, title, source }) {
   return (
     <div className="flex flex-row items-center">
-      <div className="flex flex-col leading-[1]">
+      {/* date */}
+      <div className="flex flex-col items-center leading-[1] min-w-[90px]">
         <p className="text-[52px] sm:text-[52px] lg:text-[64px]">{day}</p>
         <p className="text-[14px] lg:text-[20px]">{date}</p>
       </div>
+
+      {/* divider */}
       <div className="w-[5px] h-[100px] bg-white rotate-[15deg] mx-6"></div>
+
+      {/* info */}
       <div>
         <p className="text-[18px] sm:text-[20px] lg:text-[24px] font-semibold">{title}</p>
         <p className="text-[18px] sm:text-[20px] lg:text-[24px] italic">{source}</p>
--- src/components/PressReleases.jsx
+++ src/components/PressReleases.jsx
@@ -12,21 +12,51 @@ function PressReleases() {
       <p className="text-[40px] sm:text-[48px] lg:text-[64px] font-semibold pb-6">Press Releases</p>
       <div className="flex flex-col gap-[18px]">
         <PressItem
-          day="01"
-          date="MAY 25'"
+          day="12"
+          date="FEB 25'"
           title="Purdue RAs discuss unionizing as overcrowded dorms cause concern"
+          source="WNDU"
+        />
+        <PressItem
+          day="11"
+          date="FEB 25'"
+          title="Purdue's dorms could be home to Indiana's first undergraduate workers' union"
+          source="Star City News"
+        />
+        <PressItem
+          day="11"
+          date="FEB 25'"
+          title="Protesters gather at Smalley Center as admin meets with RA union"
           source="The Exponent"
         />
         <PressItem
-          day="01"
-          date="MAY 25'"
-          title="Purdue RAs discuss unionizing as overcrowded dorms cause concern"
-          source="The Exponent"
+          day="11"
+          date="FEB 25'"
+          title="RAs at Purdue bringing attention to overcrowded dowms, discussing unionizing"
+          source="WTHR Indianapolis"
         />
         <PressItem
-          day="01"
-          date="MAY 25'"
-          title="Purdue RAs discuss unionizing as overcrowded dorms cause concern"
+          day="09"
+          date="FEB 25'"
+          title="Purdue dorm RAs push case for a union"
+          source="Pressreader"
+        />
+        <PressItem
+          day="06"
+          date="FEB 25'"
+          title="Purdue Resident Assistants Push for Unionization"
+          source="WLFI"
+        />
+        <PressItem
+          day="04"
+          date="FEB 25'"
+          title="Purdue dorm resident assistants push their case for a union; top concern is sharing rooms"
+          source="Journal & Courier"
+        />
+        <PressItem
+          day="27"
+          date="JAN 25'"
+          title="Purdue RAs take first steps twoard unionization"
           source="The Exponent"
         />
       </div>
