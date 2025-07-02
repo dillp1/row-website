da6b315cff546b64029220d38cacd45f1f85a0e1
dillp1 <dillon.michlena@gmail.com>
2025-07-02 10:50:20 -0400

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ function Footer() {
     <footer className="footer flex flex-col sm:flex-row justify-between items-center px-6 py-4 md:px-[75px] lg:px-[150px] gap-4 sm:gap-0">
       <p className="text-[40px] sm:text-[48px] lg:text-[64px] font-semibold">ROW</p>
       <div className="flex flex-row gap-[20px]">
-        <img src={instagramIcon} className="w-8 h-auto lg:w-16 lg:h-auto"/>
-        <img src={mailIcon} className="w-8 h-auto lg:w-16 lg:h-auto"/>
+        <img src={instagramIcon} className="w-8 h-8 sm:w-10 sm:h-10" />
+        <img src={mailIcon} className="w-8 h-8 sm:w-10 sm:h-10" />
       </div>
     </footer>
   );
