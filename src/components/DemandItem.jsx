16bc52ccf4435a6c078bbcbd0673ec86efeb6a2b
dillp1 <dillon.michlena@gmail.com>
2025-07-02 16:23:48 -0400

    updated demands formatting

--- src/components/DemandItem.jsx
+++ src/components/DemandItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DemandItem({ icon, text }) {
   return (
-    <div className="flex flex-row items-center gap-[15px]">
+    <div className="flex flex-row items-center md:items-start lg:items-center text-left gap-[15px]">
       <img src={icon} alt=""/>
       <p className="text-[18px] sm:text-[20px] lg:text-[24px]">{text}</p>
     </div>
