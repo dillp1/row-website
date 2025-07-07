bc275ca080462511f66e2eb81ec0c9f31e85d7fb
dillp1 <dillon.michlena@gmail.com>
2025-07-07 11:16:05 -0400

    chevron now used lucide icon

--- src/components/FAQItem.jsx
+++ src/components/FAQItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
 import { useState } from "react";
+
+import { ChevronDown } from "lucide-react";
 
 function FAQItem({ question, answer }) {
   const [open, setOpen] = useState(false);
@@ -12,13 +13,9 @@ function FAQItem({ question, answer }) {
     >
       <div className="flex justify-between items-center text-[18px] sm:text-[20px] lg:text-[24px] font-semibold">
         <p>{question}</p>
-        <img
-          src="src/assets/chevron.svg"
-          alt="chevron"
-          className={`w-6 h-6 ml-6 transition-transform duration-300 ${
+        <ChevronDown className={`w-10 h-10 ml-6 transition-transform duration-300 ${
             open ? "rotate-180" : ""
-          }`}
-        />
+          }`}/>
       </div>
       {open && (
         <div className="text-[16px] sm:text-[17px] lg:text-[18px] pt-[15px] text-white">
