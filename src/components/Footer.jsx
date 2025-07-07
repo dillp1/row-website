3ae8ec0d5005a068f03c098b90103e61e7073abb
Dillon Michlena <dillon.michlena@gmail.com>
2025-07-07 14:26:35 -0400

    footer now has proper links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,16 @@ function Footer() {
     <footer className="footer flex flex-col sm:flex-row justify-between items-center px-6 py-4 md:px-[75px] lg:px-[150px] gap-4 sm:gap-0">
       <p className="text-[40px] sm:text-[48px] lg:text-[64px] font-semibold">ROW</p>
       <div className="flex flex-row gap-[20px]">
-        <div className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12">
-          <Instagram className="w-full h-full" />
-        </div>
-        <div className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12">
-          <Mail className="w-full h-full" />
-        </div>
+        <a href="https://instagram.com/rowatpurdue">
+          <div className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12">
+            <Instagram className="w-full h-full" />
+          </div>
+        </a>
+        <a href="mailto:row.purdue.uni@gmail.com">
+          <div className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12">
+            <Mail className="w-full h-full" />
+          </div>
+        </a>
       </div>
     </footer>
   );
