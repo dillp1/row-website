613b6fe61b8b4e0a0802c620f1770f4aac9d8c52
dillp1 <dillon.michlena@gmail.com>
2025-07-07 10:59:36 -0400

    demand items use lucidde icons

--- src/components/DemandItem.jsx
+++ src/components/DemandItem.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { BadgeCheck, CircleAlert, Megaphone } from "lucide-react";
 
-function DemandItem({ icon, text }) {
+function DemandItem({ status, text }) {
+  let Icon;
+  let bgColor;
+  switch (status) {
+    case "met":
+      Icon = BadgeCheck;
+      bgColor = "bg-green-400"
+      break;
+    case "addressed":
+      Icon = Megaphone;
+      bgColor = "bg-yellow-400"
+      break;
+    case "unaddressed":
+    default:
+      Icon = CircleAlert;
+      bgColor = "bg-red-400"
+      break;
+  }
+
   return (
     <div className="flex flex-row items-center md:items-start lg:items-center text-left gap-[15px]">
-      <img src={icon} alt=""/>
+      <div className={`w-14 h-14 sm:w-12 sm:h-12 lg:w-18 lg:h-18 min-w-14 sm:min-w-12 lg:min-w-18 rounded-full ${bgColor} text-black flex items-center justify-center`}>
+        <Icon className="w-8 h-8 sm:w-6 sm:h-6 lg:w-10 lg:h-10" />
+      </div>
       <p className="text-[18px] sm:text-[20px] lg:text-[24px]">{text}</p>
     </div>
   );
--- src/components/Demands.jsx
+++ src/components/Demands.jsx
@@ -2,10 +2,6 @@ import React from "react";
 
 import DemandItem from "./DemandItem";
 
-import metIcon from "../assets/MetNotification.svg";
-/* import addressedIcon from "../assets/AddressedNotification.svg"; */
-import unaddressedIcon from "../assets/UnaddressedNotification.svg";
-
 function Demands() {
   return (
     <section
@@ -16,51 +12,51 @@ function Demands() {
       <p className="text-[40px] sm:text-[60px] lg:text-[64px] font-semibold pb-[25px]">We Demand...</p>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-[40px]">
         <DemandItem
-          icon={metIcon}
+          status="met"
           text="... that any ResLife student staff never be assigned to share a living space with residents or a bedroom with any other individuals."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... a guaranteed severance package for any ResLife student staff who resigns or is fired. This should include, but not be limited to, the option of housing and a meal plan for the remainder of the given academic year."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... any ResLife student staff required to use personal vehicles to do their job in a timely manner be provided vehicle reimbursements consistent with the IRS standard mileage rates."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... any ResLife student staff required to live in housing without air conditioning be provided with box fans."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... all ResLife student staff be guaranteed free parking."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... that contracts will move from at-will employment to just-cause employment, and will not change without mutual agreement after being signed and accepted."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... every ResLife student staff member be able to choose which meal plan they want."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... at most 40 residents be assigned to any given RA."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... at least one mental health counselor to be on call for all ResLife student staff who is not a Title IX mandatory reporter."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... to no longer be compelled to work front desk shifts without additional compensation."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... transparency regarding traumatic events and deaths within the community."
         />
         <DemandItem
-          icon={unaddressedIcon}
+          status="unaddressed"
           text="... free laundry for all ResLife student staff."
         />
       </div>
