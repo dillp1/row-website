ed414ef831ee9d793c053585ec65ef8e3803cdc7
Dillon Michlena <144554032+dillp1@users.noreply.github.com>
2025-08-27 00:30:05 -0400

    adds min width and height to chevrons

--- src/components/FAQItem.jsx
+++ src/components/FAQItem.jsx
@@ -13,9 +13,11 @@ function FAQItem({ question, answer }) {
     >
       <div className="flex justify-between items-center text-[18px] sm:text-[20px] lg:text-[24px] font-semibold">
         <p>{question}</p>
-        <ChevronDown className={`w-10 h-10 ml-6 transition-transform duration-300 ${
+        <ChevronDown
+          className={`min-w-10 min-h-10 ml-6 transition-transform duration-300 ${
             open ? "rotate-180" : ""
-          }`}/>
+          }`}
+        />
       </div>
       {open && (
         <div className="text-[16px] sm:text-[17px] lg:text-[18px] pt-[15px] text-white">
